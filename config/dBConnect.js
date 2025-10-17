a78f23f910aaa8ba7bb22f30b000837a9b031209
Gunvant Panchal <gunvantpanchal2001@gmail.com>
2025-10-17 13:53:00 +0530

    fix: redeployment

--- config/dBConnect.js
+++ config/dBConnect.js
@@ -5,7 +5,7 @@ const URL = "mongodb+srv://alessandro:RPrxd9ZCKyUt0EKy@anyma.ze4sr.mongodb.net/A
 const Connection = async () => {
   try {
     await mongoose.connect(process.env.DB_URL || URL);
-    console.log("Database connected successfully");
+    console.log("Database connected successfully - test");
   } catch (error) {
     console.log(error.message);
   }
